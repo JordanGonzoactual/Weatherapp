96c9e55b3197b5e6c6885f7c0cb9932a0bde964d
Jordan Gonzalez <Jordanaigonzalez@gmail.com>
2024-08-01 21:34:55 -0400

    cyan bg

--- app.py
+++ app.py
@@ -3,13 +3,24 @@ from tkinter import ttk
 import threading
 import unittest
 
+#Window
+root = tk.Tk()
 
 #Application class
 class Weatherapp(tk.Tk):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, master):
+        super().__init__(master)
         self.title("Weatherapp")
         self.geometry("1900x1080")
+        self.configure(bg='#5DE2E7')
+        self.add_widgets()
+    
+
+    def add_widgets(root):
+        button = tk.Button(root, text="Click me")
+        button.pack()
+
+
 
 
         
@@ -22,12 +33,5 @@ class Weatherapp(tk.Tk):
 
 
 
-
-
-
-#Window
-root = tk.Tk()
-myLabel = tk.Label(root, text= "Hello World")
-myLabel.pack
-
+root.configure(bg='cyan')
 root.mainloop()
