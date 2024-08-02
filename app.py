f9ae79d73b283f12030189e8e7b0a9119a0658b8
Jordan Gonzalez <Jordanaigonzalez@gmail.com>
2024-08-01 20:28:09 -0400

    Added app class

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 import tkinter as tk
 from tkinter import ttk 
 import threading
+import unittest
+
+
+#Application class
+class Weatherapp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Weatherapp")
+        self.geometry("1900x1080")
+
+
+        
+
+
+
+
+
+
+
+
 
 
 
@@ -8,3 +28,6 @@ import threading
 #Window
 root = tk.Tk()
 myLabel = tk.Label(root, text= "Hello World")
+myLabel.pack
+
+root.mainloop()
