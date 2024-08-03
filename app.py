0434650905d13dd18b7ff437cd28edd7e9e47c26
Jordan Gonzalez <Jordanaigonzalez@gmail.com>
2024-08-02 20:09:48 -0400

    added logging functions

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from tkinter import *
 from PIL import Image, ImageTk
 import logging
 
-
-
+# Logging processes
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 #Application class
 class Weatherapp(tk.Tk):
@@ -24,23 +24,30 @@ class Weatherapp(tk.Tk):
     def add_widgets(self):
         # Frame for Drop down menu
         self.Dpframe = tk.Frame(self, bg='#FFFFFF', height=150, width=125, relief= RAISED)
+        logging.debug("Creating and packing Dpframe")
         self.Dpframe.pack(padx=30, pady=30, fill= 'both', expand= True)
+        logging.debug(f"Frame packed with properties: bg={self.Dpframe.cget('bg')}, 
+                      width={self.Dpframe.winfo_width()}, height={self.Dpframe.winfo_height()}")
         
         #Drop down Menu
+        logging.debug("Creating and packing Dropdown")
         options = ["Monday"," Tuesday", "Wednesday", "Thursday","Friday","Saturday","Wednesday"]
         clicked = tk.StringVar()
         clicked.set("Monday")
         self.dropdown = tk.OptionMenu(self.Dpframe, clicked, *options)
         self.dropdown.config(relief= RAISED, width=50, bg='#E4080A')
         self.dropdown.pack(padx=30, pady=30, fill='both', expand=True)
-    
+        logging.debug(f"Dropdown created with properties: bg={self.dropdown.cget('bg')}, width={self.dropdown.winfo_width()}")
+
+
     def add_background(self):
         self.bgimage='H:/Python/Weatherapp/Images/Weatherbg.jpg'
-        print(f"Icon path: {self.bgimage}")
+        logging.debug(f"Image path: {self.bgimage}")
         self.image = Image.open(self.bgimage)
         self.background= ImageTk.PhotoImage(self.image)
         self.background_label = tk.Label(self, image=self.background)
         self.background_label.pack(fill='both',expand=True)
+        logging.debug("Background image added")
         
        
 
